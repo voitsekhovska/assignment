59da37f59dcc86e099e4b758da273e71a359c2a2
ye.voitsekhovska <sunakojane6@gmail.com>
2025-12-12 21:46:34 +0200

    code refactored

--- index.js
+++ index.js
@@ -1,55 +1,24 @@
-const input = [
-  "Square TopRight 1 1 Side 1",
-  "Rectangle TopRight 2 2 BottomLeft 1 1",
-  "Circle Center 1 1 Radius 2",
-];
+import { shapeCalc } from "./shapes.js";
 
-const squareCalc = (items) => {
-  const side = Number(items[items.length - 1]);
+const input = document.getElementById("shapeInput");
+const button = document.querySelector(".btn");
+const output = document.querySelector(".results");
 
-  return {
-    perimeter: 4 * side,
-    area: side * side,
-  };
-};
+const calculateShape = () => {
+  const line = input.value;
+  if (!line) return;
 
-const rectangleCalc = (items) => {
-  const x1 = Number(items[2]);
-  const y1 = Number(items[3]);
-  const x2 = Number(items[5]);
-  const y2 = Number(items[6]);
+  const shape = shapeCalc(line);
+  if (!shape) {
+    output.textContent = "Invalid shape type";
+  }
 
-  const width = Math.abs(x1 - x2);
-  const heigh = Math.abs(y1 - y2);
-
-  return {
-    perimeter: (width + heigh) * 2,
-    area: width * heigh,
-  };
-};
-
-const circleCalc = (items) => {
-  const radius = Number(items[items.length - 1]);
-  const pi = 3.14;
-
-  return {
-    perimeter: 2 * pi * radius,
-    area: pi * Math.pow(radius, 2),
-  };
-};
-
-const shapeCalc = (line) => {
-  const parts = line.split(" ");
-  const type = parts[0];
-
-  if (type === "Square") return squareCalc(parts);
-  if (type === "Rectangle") return rectangleCalc(parts);
-  if (type === "Circle") return circleCalc(parts);
-};
-
-for (const line of input) {
+  const perimeter = shape.getPerimeter().toFixed();
+  const area = shape.getArea().toFixed();
   const type = line.split(" ")[0];
-  const { perimeter, area } = shapeCalc(line);
 
-  console.log(`${type} Perimeter ${perimeter} Area ${area}`);
-}
+  output.textContent = `${type} Perimeter ${perimeter} Area ${area}`;
+  input.value = "";
+};
+
+button.addEventListener("click", calculateShape);
---
+++ shapes.js
@@ -0,0 +1,70 @@
+class Shape {
+  getPerimeter() {}
+  getArea() {}
+}
+
+class Square extends Shape {
+  constructor(side) {
+    super();
+    this.side = side;
+  }
+
+  getPerimeter() {
+    return 4 * this.side;
+  }
+  getArea() {
+    return this.side * this.side;
+  }
+}
+
+class Rectangle extends Shape {
+  constructor(x1, y1, x2, y2) {
+    super();
+    this.width = Math.abs(x1 - x2);
+    this.heigh = Math.abs(y1 - y2);
+  }
+
+  getPerimeter() {
+    return 2 * (this.width + this.heigh);
+  }
+  getArea() {
+    return this.width * this.heigh;
+  }
+}
+
+class Circle extends Shape {
+  constructor(radius) {
+    super();
+    this.radius = radius;
+  }
+
+  getPerimeter() {
+    return 2 * Math.PI * this.radius;
+  }
+  getArea() {
+    return Math.Pi * this.radius * this.radius;
+  }
+}
+
+const shapeCalc = (line) => {
+  const parts = line.split(" ");
+  const type = parts[0];
+
+  switch (type) {
+    case "Square":
+      return new Square(Number(parts[parts.length - 1]));
+    case "Rectangle":
+      return new Rectangle(
+        Number(parts[2]),
+        Number(parts[3]),
+        Number(parts[5]),
+        Number(parts[6])
+      );
+    case "Circle":
+      return new Circle(Number(parts[parts.length - 1]));
+    default:
+      return;
+  }
+};
+
+export { shapeCalc };
